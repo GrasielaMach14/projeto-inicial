a8ecdec8cc5d0027a832bf3f760eaac8588388d6
Grasiela Machado <aciatuani@gmail.com>
2020-01-24 02:05:02 -0300

    Continuação do exercício - exclusão

--- view/cursos/formulario.php
+++ view/cursos/formulario.php
@@ -1,6 +1,6 @@
 <?php include __DIR__ . '/../inicio.php';?>
 
-    <form action="/salvar_curso" method="post">
+    <form action="/salvar-curso" method="post">
         <div class="form-group">
             <label for="descricao">Descrição</label>
             <input type="text" id="descricao" name="descricao" class="form-control">
